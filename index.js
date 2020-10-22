43a76ae826e43fbf41845aa62691cce1752c0878
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-10-22 11:10:07 -0300

    #16 Movie and Client created!

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.get("/", (req, res) => {
 
 //Route movie import
 require("./Routes/movie.routes")(app);
+require("./Routes/client.routes")(app);
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
 });
